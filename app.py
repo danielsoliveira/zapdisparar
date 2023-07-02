7e002a59a43810a5bb387b902dad72549ea62e4a
Daniel Oliveira <danielfinal@gmail.com>
2023-07-02 14:58:09 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import pandas as pd
+
+from openpyxl import Workbook
+from io import StringIO
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from webdriver_manager.core.utils import ChromeType
+
+option = webdriver.ChromeOptions()
+option.add_argument("start-maximized")
+
+#from webdriver_manager.firefox import GeckoDriverManager
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import urllib
+import time
+import os
+import random
+
+st.title('ZapDisparar')
+st.header('Lista de transmissão')
+lista  = st.file_uploader("Selecione lista de contatos", type=["xlsx","xls"])
+if lista is not None:
+    tabela = pd.read_excel(lista)
+    st.write(tabela)
+botao = st.button('Enviar')
+    
+if botao and lista is not None:
+
+    navegador = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=option)
+    navegador.get("https://web.whatsapp.com")
+
+    # esperar a tela do whatsapp carregar
+    while len(navegador.find_elements(By.ID, 'side')) < 1: # -> lista for vazia -> que o elemento não existe ainda
+        time.sleep(1)
+    time.sleep(2) # só uma garantia
+
+    for linha in tabela.index:
+        # enviar uma mensagem para a pessoa
+        nome = tabela.loc[linha, "nome"]
+        mensagem = tabela.loc[linha, "mensagem"]
+        imagem = tabela.loc[linha, "imagem"]
+        arquivo = tabela.loc[linha, "arquivo"]
+        telefone = tabela.loc[linha, "telefone"]
+        
+        texto = mensagem.replace("fulano", nome)
+        texto = urllib.parse.quote(texto)
+
+        # enviar a mensagem
+        link = f"https://web.whatsapp.com/send?phone={telefone}&text={texto}"
+        
+        navegador.get(link)
+        # esperar a tela do whatsapp carregar -> espera um elemento que só existe na tela já carregada aparecer
+        while len(navegador.find_elements(By.ID, 'side')) < 1: # -> lista for vazia -> que o elemento não existe ainda
+            time.sleep(1)
+        time.sleep(2) # só uma garantia
+        
+        # você tem que verificar se o número é inválido
+        if len(navegador.find_elements(By.XPATH, '//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div[1]')) < 1:
+            
+            # enviar a mensagem
+            navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span').click()
+            
+            if imagem != "N":
+                caminho_completo = os.path.abspath(f"C:/docs/{imagem}")
+                navegador.find_element(By.XPATH, 
+                                    '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div/span').click()
+                navegador.find_element(By.XPATH, 
+                                    '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]/button/input').send_keys(caminho_completo)
+                time.sleep(3)
+                navegador.find_element(By.XPATH, 
+                                    '//*[@id="app"]/div/div/div[3]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div/span').click()
+            
+                time.sleep(1)
+            
+            if arquivo != "N":
+                caminho_completo = os.path.abspath(f"C:/docs/{arquivo}")
+                navegador.find_element(By.XPATH,
+                                    '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div/span').click()
+                navegador.find_element(By.XPATH, 
+                                    '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button/input').send_keys(caminho_completo)
+                time.sleep(3)
+                navegador.find_element(By.XPATH, 
+                                    '//*[@id="app"]/div/div/div[3]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div/span').click()
+                
+
+            time.sleep(random.randrange(5,8))
